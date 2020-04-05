168ce5efd389c86da187f968edcdb42b57eb410e
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-05 22:51:03 +0200

    add transactional annotation

--- src/main/java/com/Transaction/Transaction/RestController/RestController.java
+++ src/main/java/com/Transaction/Transaction/RestController/RestController.java
@@ -5,6 +5,7 @@ import com.Transaction.Transaction.Services.SimpleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
     @Autowired
     private SimpleService simpleService;
--- src/main/java/com/Transaction/Transaction/Services/SimpleService.java
+++ src/main/java/com/Transaction/Transaction/Services/SimpleService.java
@@ -2,17 +2,20 @@ package com.Transaction.Transaction.Services;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Scope("singleton")
 public class SimpleService {
     private int index = 0;
 
+    @Transactional
     public void incrementIndex() {
         this.index++;
         System.out.println(this.index);
     }
 
+    @Transactional
     public void decrementIndex() {
         this.index--;
         System.out.println(this.index);
